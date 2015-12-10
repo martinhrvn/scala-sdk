cbf4ab80fdd63fb3ddad4eb61209ebaf4ba4cdb9
Martin Harvan <martinhrvn@gmail.com>
2015-12-10 17:38:01 +0100

    Added JavaDoc for LanguageTranslation service

--- src/main/scala/com/ibm/watson/developercloud/language_translation/v2/LanguageTranslation.scala
+++ src/main/scala/com/ibm/watson/developercloud/language_translation/v2/LanguageTranslation.scala
@@ -1,35 +1,23 @@
 package com.ibm.watson.developercloud.language_translation.v2
 
-import java.io.{BufferedInputStream, FileInputStream}
-import java.util.concurrent.{Executors, ExecutorService}
-
-import LanguageTranslationProtocol._
-import com.ibm.watson.developercloud.utils.{JsonUtils, Validation, WatsonServiceConfig, WatsonService}
+import com.ibm.watson.developercloud.language_translation.v2.LanguageTranslationProtocol._
+import com.ibm.watson.developercloud.utils.{JsonUtils, Validation, WatsonService, WatsonServiceConfig}
 import com.typesafe.scalalogging.LazyLogging
-import spray.can.Http
 import spray.client.pipelining._
-import spray.client._
-import spray.http.{BasicHttpCredentials, Uri, HttpRequest}
-import spray.http.Uri.Query
-import spray.httpx.unmarshalling._
-import spray.io.ClientSSLEngineProvider
-import spray.json.{JsObject, JsString, JsValue}
-import spray.httpx.SprayJsonSupport._
-
-import scala.concurrent.{ExecutionContext, Future}
-import scala.concurrent.duration._
-
-import akka.actor.ActorSystem
-import akka.util.Timeout
-import akka.pattern.ask
-import akka.io.IO
-
-import spray.can.Http
 import spray.http._
-import HttpMethods._
+import spray.httpx.SprayJsonSupport._
+import spray.httpx.unmarshalling._
+import spray.json.{JsObject, JsString, JsValue}
+
+import scala.concurrent.Future
+
 /**
- * Created by harvan on 22.7.2015.
- */
+  * The IBM Watson Language Translation service translate text from one language to another and
+  * identifies the language in which text is written.
+  *
+  * @version v2
+  * @param config service config
+  */
 class LanguageTranslation(config: WatsonServiceConfig ) extends WatsonService(config) with LazyLogging {
   import system.dispatcher
 
@@ -40,8 +28,20 @@ class LanguageTranslation(config: WatsonServiceConfig ) extends WatsonService(co
   val PATH_IDENTIFIABLE_LANGUAGES: String = "/v2/identifiable_languages"
   val PATH_MODELS: String = "/v2/models"
   def serviceType = "language_translation"
+
+  /**
+    * Retrieves a list of models
+    * @return future of language models
+    */
   def getModels : Future[LanguageModels] = getModels(Map())
 
+  /**
+    * Retrieves a translation models.
+    * @param showDefault show default models
+    * @param source the source language
+    * @param target the target language
+    * @return the translation models
+    */
   def getModels(showDefault: Boolean, source: String, target: String) : Future[LanguageModels] = {
 
     val map = collection.mutable.Map[String, String]()
@@ -64,6 +64,11 @@ class LanguageTranslation(config: WatsonServiceConfig ) extends WatsonService(co
     getModels(map.toMap)
   }
 
+  /**
+    * Retrieves the list of models.
+    * @param params list of parameters to search for models
+    * @return future
+    */
   def getModels(params: Map[String,String]) : Future[LanguageModels] = {
     logger.info("Entering getModels")
 
@@ -73,6 +78,12 @@ class LanguageTranslation(config: WatsonServiceConfig ) extends WatsonService(co
     response.map(unmarshal[LanguageModels])
   }
 
+  /**
+    * Creates a translation models.
+    *
+    * @param options the create model options
+    * @return the translation model
+    */
   def createModel(options: CreateModelOptions) : Future[LanguageModel] = {
     var data : List[BodyPart] = List(BodyPart(options.baseModelId, formHeaders("name" -> "body_part")))
     data = Option(options.forcedGlossary) match {
@@ -102,6 +113,11 @@ class LanguageTranslation(config: WatsonServiceConfig ) extends WatsonService(co
 
   }
 
+  /**
+    * Deletes a model by ID
+    * @param modelId identifier of model
+    * @return future of HttpRequest
+    */
   def deleteModel(modelId : String) : Future[HttpResponse] = {
     val id : String = Option(modelId) match {
       case Some(mid) if mid.nonEmpty => mid
@@ -111,29 +127,62 @@ class LanguageTranslation(config: WatsonServiceConfig ) extends WatsonService(co
     send(request)
   }
 
+
+  /**
+    * Retrieves a list of identifiable languages
+    * @return the list of identifiable languages
+    */
   def getIdentifiableLanguages() : Future[List[IdentifiableLanguage]] = {
     val request = Get(endpoint + PATH_IDENTIFIABLE_LANGUAGES)
     val response = send(request)
     response.map(unmarshal[List[IdentifiableLanguage]])
   }
 
+  /**
+    * Identifies language in which text is written.
+    * @param text the text to identify
+    * @return the identified language
+    */
   def identify (text: String): Future[List[IdentifiedLanguage]] = {
     val request = Post(endpoint + PATH_IDENTIFY, text)
     val response = send(request)
     response.map(unmarshal[List[IdentifiedLanguage]])
   }
 
+  /**
+    * Translates text using model
+    * @param text text to translate
+    * @param modelId the model ID
+    * @return translation result
+    */
   def translate(text: String, modelId: String): Future[TranslationResult] = {
     Validation.notEmpty(modelId, "ModelId cannot be empty")
     translateRequest(text, modelId, null, null)
   }
 
+  /**
+    * Translate text using source and target languages
+    * @param text text to translate
+    * @param source source language
+    * @param target target language
+    * @return translated text
+    */
   def translate(text: String, source: String, target: String) : Future[TranslationResult]= {
     Validation.notEmpty(source, "Source cannot be empty")
     Validation.notEmpty(target, "Target cannot be empty")
     translateRequest(text, null, source, target)
   }
 
+  /**
+    * Translate paragraphs of text using a model and or source and target. model_id or source and
+    * target needs to be specified. If both are specified, then only model_id will be used
+    *
+    * @param text text to translate
+    * @param modelId id of the model
+    * @param source source language
+    * @param target target language
+    * @return translated text
+    */
   def translateRequest(text: String, modelId: String, source: String, target: String) : Future[TranslationResult] = {
     Validation.notEmpty(text, "Text cannot be empty")
     var map : Map[String, JsValue] = Map("text" -> JsString(text))
