d80cd0cfeee038a06217992bf4c39ea2c33df1a1
Martin Harvan <martinhrvn@gmail.com>
2015-12-10 16:49:35 +0100

    Added javadoc for NaturalLanguageClassifier

--- src/main/scala/com/ibm/watson/developercloud/natural_language_classifier/v1/NaturalLanguageClassifier.scala
+++ src/main/scala/com/ibm/watson/developercloud/natural_language_classifier/v1/NaturalLanguageClassifier.scala
@@ -1,7 +1,7 @@
 package com.ibm.watson.developercloud.natural_language_classifier.v1
 
 import com.ibm.watson.developercloud.natural_language_classifier.v1.NaturalLanguageClassifierProtocol._
-import com.ibm.watson.developercloud.utils.{Validation, WatsonServiceConfig, WatsonService}
+import com.ibm.watson.developercloud.utils.{JsonUtils, Validation, WatsonServiceConfig, WatsonService}
 import com.typesafe.scalalogging.LazyLogging
 import spray.client.pipelining._
 import spray.http._
@@ -11,7 +11,14 @@ import spray.json._
 import scala.concurrent.Future
 
 
-
+/**
+  * The IBM Watson Natural Language Classifier service applies deep learning techniques to make
+  * predictions about the best predefined classes for short sentences or phrases. The classes can
+  * trigger a corresponding action in an application, such as directing a request to a location or
+  * person, or answering a question. After training, the service returns information for texts that
+  * it hasn't seen before. The response includes the name of the top classes and confidence values.
+  * @param config Configuration for service
+  */
 class NaturalLanguageClassifier(config: WatsonServiceConfig) extends WatsonService(config) with LazyLogging {
   import system.dispatcher
   def serviceType = "natural_language_classifier"
@@ -19,12 +26,24 @@ class NaturalLanguageClassifier(config: WatsonServiceConfig) extends WatsonServi
   val CLASSIFIER_URL = "/v1/classifiers/%s"
   val CLASSIFICATION_URL = "/v1/classifiers/%s/classify"
 
-
-  def createClassifier(language: String, trainingData: List[TrainingData]): Future[Classifier] = {
+  /**
+    * Sends data to create and train a classifier, and returns information about the new classifier.
+    * The status has the value of `Training` when the operation is successful, and might remain at
+    * this status for a while.
+    *
+    * @param name name of the classifier
+    * @param language IETF primary language for the classifier. for example: 'en'
+    * @param trainingData The set of questions and their "keys" used to adapt a system to a domain
+    *        (the ground truth)
+    * @return created classifier
+    */
+  def createClassifier(name: String, language: String, trainingData: List[TrainingData]): Future[Classifier] = {
     var map = Map[String, JsValue]()
     Validation.notEmpty(language, "Language cannot be empty")
+
     //TODO: Validation.notEmpty(trainingData, "Training data cannot be empty")
     map + ("language" -> JsString(language))
+    map = JsonUtils.addIfNotEmpty(name, "name", map)
 
     map = Option(trainingData) match {
       case Some(testData) if testData.nonEmpty => map + ("training_data" -> trainingData.toJson)
@@ -37,6 +56,13 @@ class NaturalLanguageClassifier(config: WatsonServiceConfig) extends WatsonServi
     send(post).map(unmarshal[Classifier])
   }
 
+  /**
+    * Returns classification information for a classifier on a phrase.
+    *
+    * @param classifierId the classifier id
+    * @param text the submitted phrase to classify
+    * @return the classification of the phrase with a given classifier
+    */
   def classify(classifierId : String, text: String) : Future[Classification] = {
     Validation.notEmpty(classifierId, "Classifier ID cannot be empty")
     Validation.notEmpty(text, "Text cannot be empty")
@@ -48,6 +74,11 @@ class NaturalLanguageClassifier(config: WatsonServiceConfig) extends WatsonServi
     send(request).map(unmarshal[Classification])
   }
 
+  /**
+    * Retrieves the list of classifiers for the user.
+    *
+    * @return the classifier list
+    */
   def getClassifiers : Future[List[Classifier]] = {
     logger.info("Running getClassifiers method")
     val response: Future[HttpResponse] = send(Get(endpoint + CLASSIFIERS_URL))
@@ -55,6 +86,12 @@ class NaturalLanguageClassifier(config: WatsonServiceConfig) extends WatsonServi
     response.map(unmarshal[List[Classifier]])
   }
 
+  /**
+    * Deletes a classifier
+    *
+    * @param classifierId the classifier ID
+    * @return future of HttpResponse
+    */
   def deleteClassifier(classifierId : String) : Future[HttpResponse] = {
     Validation.notEmpty(classifierId, "Classifier ID cannot be empty")
     val url = CLASSIFIER_URL.format(classifierId)
@@ -63,6 +100,11 @@ class NaturalLanguageClassifier(config: WatsonServiceConfig) extends WatsonServi
     response
   }
 
+  /**
+    * Retrieves a classifier by ID
+    * @param classifierId ID of the classifier
+    * @return Classifier
+    */
   def getClassifier(classifierId : String) : Future[Classifier] = {
     Validation.notEmpty(classifierId, "Classifier ID cannot be empty")
 
