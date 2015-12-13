9a3357fd045a7574d7a0ac08f8a18fddaee38d1a
Martin Harvan <martinhrvn@gmail.com>
2015-12-13 23:20:42 +0100

    Adding fileExists method to Validation class

--- src/main/scala/com/ibm/watson/developercloud/utils/Validation.scala
+++ src/main/scala/com/ibm/watson/developercloud/utils/Validation.scala
@@ -1,9 +1,19 @@
 package com.ibm.watson.developercloud.utils
 
+import java.io.File
+
+import com.ibm.watson.developercloud.visual_insights.v1.Summary
+
+import scala.concurrent.Future
+
 /**
   * Created by martinhrvn on 10/12/15.
   */
 object Validation {
+  def fileExists(file: File, message: String) = {
+    validate(file, {p : File => Option(p) match {case Some(p) if p.exists => true; case _ => false}}, message)
+  }
+
   def validate[T](value: T, condition: T => Boolean, message: String) = {
     if(!condition(value)) {
       throw new IllegalArgumentException(message)
