77a5b83e206aa2f6956b4dd804f6bd6e9b3a6d00
Martin Harvan <martinhrvn@gmail.com>
2015-12-13 23:50:35 +0100

    Javadoc for Validation

--- src/main/scala/com/ibm/watson/developercloud/utils/Validation.scala
+++ src/main/scala/com/ibm/watson/developercloud/utils/Validation.scala
@@ -7,19 +7,36 @@ import com.ibm.watson.developercloud.visual_insights.v1.Summary
 import scala.concurrent.Future
 
 /**
-  * Created by martinhrvn on 10/12/15.
+  * Object for validation purposes
   */
 object Validation {
+  /**
+    * Validates if file is not null and exists
+    * @param file file to validate
+    * @param message message to use for exception
+    */
   def fileExists(file: File, message: String) = {
-    validate(file, {p : File => Option(p) match {case Some(p) if p.exists => true; case _ => false}}, message)
+    validate(file, {x : File => x != null && x.exists() }, message)
   }
 
+  /**
+    * General purpose validation function. Uses provided function to validate. If the value does not match validation rules an IllegaArgumentException is thrown.
+    * @param value value to validate
+    * @param condition condition for validation
+    * @param message message to use for exception
+    * @tparam T type of value
+    */
   def validate[T](value: T, condition: T => Boolean, message: String) = {
     if(!condition(value)) {
       throw new IllegalArgumentException(message)
     }
   }
 
+  /**
+    * Checks if provided string is not null and nonEmpty
+    * @param value String to validate
+    * @param message message to use for exception
+    */
   def notEmpty(value : String, message: String) = {
     validate(value, {p : String =>
       Option(p) match {
@@ -29,10 +46,21 @@ object Validation {
     }, message)
   }
 
+  /**
+    * Simple not Null Check
+    * @param value Value to check
+    * @param message message to use for exception
+    * @tparam T type of value to validate
+    */
   def notNull[T](value : T, message: String) = {
     validate(value, {p : T => p != null}, message)
   }
 
+  /**
+    * Throws exception if the value is false
+    * @param value throws exception if false
+    * @param message message to use for exception
+    */
   def assertTrue(value: Boolean, message: String) = {
     validate(value, {_ :Boolean => value}, message)
   }
