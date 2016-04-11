f7f448e5d8a0a01c47d9b05304a08faca81bfa75
Martin Harvan <martinhrvn@gmail.com>
2016-04-11 13:38:04 +0200

    Adding type declarations to vals

--- src/main/scala/com/ibm/watson/developer_cloud/language_translation/v2/model/IdentifiableLanguage.scala
+++ src/main/scala/com/ibm/watson/developer_cloud/language_translation/v2/model/IdentifiableLanguage.scala
@@ -1,8 +1,9 @@
 package com.ibm.watson.developer_cloud.language_translation.v2.model
 
 import com.ibm.watson.developer_cloud.service.GenericModel
+import spray.json.{RootJsonFormat, DefaultJsonProtocol}
 
 /**
   * Created by Martin Harvan (martin.harvan@sk.ibm.com) on 20/03/16.
   */
-case class IdentifiableLanguage(var language: String, var name: String)extends GenericModel
+case class IdentifiableLanguage(var language: String, var name: String) extends GenericModel
